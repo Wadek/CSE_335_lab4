de4f9a1ebb8c2064f5f041638b106e4be1df8f6c
Kariniemi <wkariniemi@gmail.com>
2017-02-15 18:12:51 -0700

    load first tableView cells

--- lab4/ViewController.swift
+++ lab4/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var places = ["Atlantis","China","Mexico","Pompeii"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return places.count
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = places[indexPath.row]
+        cell.imageView!.image = UIImage(named: places[indexPath.row]+"_flag")
+        
+        return cell
+    }
+    
+    
 
 }
 
